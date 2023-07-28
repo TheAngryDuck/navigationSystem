9e99c7cae34da947dff209e32dc33c91fc13b57f
Martin Oblikas <martin.oblikas@gmail.com>
2023-07-29 02:34:05 +0300

    initial setup

---
+++ navigation/src/main/java/com/navigation/system/navigation/NavigationSystemApplication.java
@@ -0,0 +1,14 @@
+package com.navigation.system.navigation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class NavigationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavigationSystemApplication.class, args);
+	}
+
+}
---
+++ navigation/src/test/java/com/navigation/system/navigation/NavigationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navigation.system.navigation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavigationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
